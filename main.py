09b2659c4b5736aeda01bc1e0d7a6ebeaca5ee52
Nataliia <matveeva.github@mail.ru>
2025-06-07 19:01:41 +0300

    fix clasterizator

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def handler(event):
     file_key = event['input']['file_key']
     item_id = event['input']['item_id']
 
-    #file_key = 'test_file_10.txt'
-    #item_id = 1
+    #file_key = 'разговор8транс.txt'
+    #item_id = 27
 
     print(f"Получен запрос на обработку файла: {file_key}")
     print(f"ID элемента: {item_id}")
@@ -82,41 +82,53 @@ def handler(event):
         for label in id_to_label.values()
     }
     print(full_result)
+    print("Начинаем обработку полученных значений")
 
     # Обработка значений
     # 1. Название компании-контрагента company_name
+    print("1. ORG: Название компании-контрагента company_name ")
     company_name = None
     org_value = full_result.get('ORG', None)
     if org_value:
         company_name = ' '.join(org_value)  # Склеиваем все строки в одну строку
+    print(f"company_name: {org_value}")
 
     # 2. Телефон компании company_phone
+    print("2. TEL: Телефон компании company_phone")
     tel_value = full_result.get('TEL', None)
     if tel_value:
         company_phone = tel_value[0]  # Берем первый телефон из списка
     else:
         company_phone = None
+    print(f"company_phone: {company_phone}")
 
-    # 3. Тип документа doc_type
+
+    # 3. Тип документа data_type
+    print("3. DOC: Тип документа data_type")
     doc_value = full_result.get('DOC', None)
     if doc_value:
         doc_type = ' '.join(doc_value)  # Склеиваем все строки в одну строку
     else:
         doc_type = "Тип не определен"
+    print(f"data_type: {doc_type}")
 
     # 4. Предполагаемый объем документов data_value, description, word
+    print("4. VOL: Предполагаемый объем документов")
     vol_value = full_result.get('VOL', None)
     if vol_value:
         # Извлекаем только числа из строки
         numbers = re.findall(r'\d+', vol_value[0])
         data_value = int(numbers[0]) if numbers else 0  # Берем первое найденное число
-        description = vol_value[0]
+        description = ", ".join(vol_value) if vol_value else "Не распознано"
+        print(f"4.1 description: полное описание данных из VOL - {description}")
 
         # Извлекаем слово после числа
-        text = vol_value[0]
-        match = re.search(r'\d+\s+(\w+)', text)
-        word = match.group(1) if match else None
-        print(word)
+
+        # Извлекаем все слова (состоящие только из букв)
+        words = re.findall(r'\b[а-яА-Яa-zA-Z]+\b', description)
+        word = ", ".join(words) if words else "Не распознано"
+        print(f"4.2 word: только слова из VOL - {word}")
+        print(f"4.3 amount: первое число из VOL - {data_value}")
 
     else:
         data_value = 0
@@ -124,29 +136,45 @@ def handler(event):
         description = 'Запрос не определен'
 
     # 5. Должность position_value
+    print("5. POS: Должность position_value")
     position_val = full_result.get('POS', None)
     if position_val:
         position_value = position_val[0]
     else:
         position_value = None
+    print(f"position_value - {position_value}")
 
     # 7 Следующая встреча
+    print("7. MDT: Следующая встреча")
     place_val = full_result.get('MDT', None)
     if place_val:
         place_value = place_val[0]
     else:
         place_value = None
+    print(f"Следующая встреча - {place_value}")
+
 
     # 8 Имя представителя
+    print("7. NAME: Имя представителя")
     person_name_val = full_result.get('NAME', None)
     if person_name_val:
-        person_name = person_name_val[0]
+        person_name = ", ".join(person_name_val) if person_name_val else "Не распознано"
     else:
         person_name = None
+    print(f"Имя представителя - {person_name}")
 
-    print(f"{company_name}, {company_phone}, {position_value}")
-    print(f"{doc_type}, {description}, {data_value},  {word}")
-    print(f"{place_value}")
+    print("----------------------------------------------")
+    print("Подготовлены следующие данные для записи в БД:")
+    print(f"ID объекта: {item_id}")
+    print(f"Название компании: {company_name}")
+    print(f"Должность: {position_value}")
+    print(f"Описание объема: {description}")
+    print(f"Тип документа: {doc_type}")
+    print(f"Периодичность/дата: {data_value}")
+    print(f"Контактные лица: {person_name}")
+    print(f"Место действия: {place_value}")
+    print(f"Телефон компании: {company_phone}")
+
 
     # пушим данные в БД
     update_record(item_id, company_name, position_value, description, doc_type, data_value, person_name, place_value,
